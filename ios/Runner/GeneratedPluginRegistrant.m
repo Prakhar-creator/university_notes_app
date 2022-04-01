9ccfe2b762040a417eb9c87e946ce1fee8d026c9
Prakhar Creator <prakharmishra288@gmail.com>
2022-04-01 23:22:30 +0530

    Commit to main

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,35 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_native_splash/FlutterNativeSplashPlugin.h>)
+#import <flutter_native_splash/FlutterNativeSplashPlugin.h>
+#else
+@import flutter_native_splash;
+#endif
+
+#if __has_include(<google_sign_in/FLTGoogleSignInPlugin.h>)
+#import <google_sign_in/FLTGoogleSignInPlugin.h>
+#else
+@import google_sign_in;
+#endif
+
+#if __has_include(<url_launcher_ios/FLTURLLauncherPlugin.h>)
+#import <url_launcher_ios/FLTURLLauncherPlugin.h>
+#else
+@import url_launcher_ios;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterNativeSplashPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterNativeSplashPlugin"]];
+  [FLTGoogleSignInPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTGoogleSignInPlugin"]];
+  [FLTURLLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTURLLauncherPlugin"]];
+}
+
+@end
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Prakhar_Mishra\\Documents\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Prakhar_Mishra\\Desktop\\flutter\\Flutter Notes App\\flutter_notes_application" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Prakhar_Mishra\\Documents\\flutter"
+  "PROJECT_DIR=C:\\Users\\Prakhar_Mishra\\Desktop\\flutter\\Flutter Notes App\\flutter_notes_application"
+  "FLUTTER_ROOT=C:\\Users\\Prakhar_Mishra\\Documents\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Prakhar_Mishra\\Desktop\\flutter\\Flutter Notes App\\flutter_notes_application\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Prakhar_Mishra\\Desktop\\flutter\\Flutter Notes App\\flutter_notes_application"
+  "FLUTTER_TARGET=C:\\Users\\Prakhar_Mishra\\Desktop\\flutter\\Flutter Notes App\\flutter_notes_application\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Prakhar_Mishra\\Desktop\\flutter\\Flutter Notes App\\flutter_notes_application\\.dart_tool\\package_config.json"
+)
